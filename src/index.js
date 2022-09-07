dc8ecd97755483a29d14079b858d2eae4b4a218f
ozwaldorf <self@ossian.dev>
2022-09-07 03:09:02 -0400

    feat: header and minor improvements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { git_assets } from "./declarations/git_assets";
-const copyButtonLabel = `<i data-feather="copy"></i>`;
+const copyButtonLabel = '<i data-feather="copy"></i>';
 
 window.onload = async () => {
   let repos = await git_assets.list({}).then((res) => {
@@ -14,18 +14,16 @@ window.onload = async () => {
     return repos;
   });
 
-  let host;
-  if (window.location.origin.includes(".raw.ic0")) {
-    host = window.location.origin;
-  } else {
-    host = window.location.origin.replace(".ic0", ".raw.ic0");
+  let host = window.location.origin;
+  // replace with raw ic0 if not done already in host
+  if (!host.includes(".raw.ic0")) {
+    host = host.replace(".ic0", ".raw.ic0");
   }
 
   document.getElementById("repos").innerHTML = Array.from(repos)
     .map((repo) => `<pre>git clone ${host}/${repo}</pre>`)
     .join("");
 
-  // you can use a class selector instead if you, or the syntax highlighting library adds one to the 'pre'.
   let blocks = document.querySelectorAll("pre");
 
   blocks.forEach((block) => {
@@ -48,7 +46,7 @@ async function copy(event) {
   let text = pre.innerText;
   await navigator.clipboard.writeText(text);
 
-  button.innerHTML = `<i data-feather="check"></i>`;
+  button.innerHTML = '<i data-feather="check"></i>';
   feather.replace();
 
   setTimeout(() => {
